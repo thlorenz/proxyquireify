eef13183d278a2a9b7bcc7493f72798ec2a8bf2d
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-28 07:15:50 -0400

    moving proxyquire out of working bundle

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 'use strict';
-/*jshint browser: true */
 
-module.exports = function () {
-  var require_ = this;
-  return function (request, stubs) { 
-    console.log('boom', request, stubs); 
-    return require_(request);
+var stubs;
+
+function stub(stubs_) { 
+  stubs = stubs_; 
+}
+
+function reset() { 
+  stubs = null; 
+}
+
+function fillMissingKeys(mdl, original) {
+  Object.keys(original).forEach(function (key) {
+    if (!mdl[key])  mdl[key] = original[key];
+  });
+
+  return mdl;
+}
+
+var proxyquire = module.exports = function (require_) {
+  reset();
+
+  return function(request, stubs) {
+
+    // set the stubs and require dependency
+    // when stub require is invoked by the module under test it will find the stubs here
+    stub(stubs);
+    var dep = require_(request);
+    reset();
+
+    return dep;
+  };
+};
+
+proxyquire.proxy =  function (require_) {
+  return function (request) {
+    function original() {
+      return require_(request);
+    }
+
+    if (!stubs) return original();
+
+    var stub = stubs[request];
+    if (!stub) return original();
+
+    var stubWideNoCallThru = !!stubs['@noCallThru'] && stub['@noCallThru'] !== false;
+    var noCallThru = stubWideNoCallThru || !!stub['@noCallThru'];
+    return noCallThru ? stub : fillMissingKeys(stub, original());
   };
 };
