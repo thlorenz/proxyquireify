b62bc05487da1276752c131c12d8086835215627
Ben Drucker <bvdrucker@gmail.com>
2015-03-14 15:46:04 -0400

    Match proxyquireify variable when var is previously created
    CoffeeScript declares variables at the top of the scope and outputs the
    following:
    
    var p
    p = require('proxyquireify')(p)

--- test/find-dependencies.js
+++ test/find-dependencies.js
@@ -12,6 +12,13 @@ test('multiple proxyquires with different names', function (t) {
   t.end()
 })
 
+test('proxyquire var declared before setting', function (t) {
+  var src = fs.readFileSync(require.resolve('./fixtures/var-declaration'), 'utf-8');
+  var deps = findDependencies(src)
+  t.deepEqual(deps, [ './depuno' ], 'returns array with each proxyquired dep exactly once')
+  t.end()
+})
+
 test('one proxyquire in actual test', function (t) {
   var src = fs.readFileSync(require.resolve('./clientside/independent-overrides'), 'utf-8');
   var deps = findDependencies(src)
