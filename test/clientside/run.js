0d5fe82f00b2a9e12c138f59544569cae1fb220d
royriojas <roy@kno.com>
2015-07-19 22:59:42 -0700

    Do not execute required mock

--- test/clientside/run.js
+++ test/clientside/run.js
@@ -6,26 +6,56 @@ var proxyquire =  require('../..');
 var vm         =  require('vm');
 var test       =  require('tape');
 
-function run(name) {
-
-  var src = '';
-
+function compile(name, cb) {
   browserify()
     .plugin(proxyquire.plugin)
     .require(require.resolve('../..'), { expose: 'proxyquireify' })
     .require(require.resolve('./' + name), { entry: true })
-    .bundle()
-    .on('error',  function error(err) { console.error(err); process.exit(1); })
-    .on('data', function (data) { src += data })
-    .on('end', function () {
-      // require('fs').writeFileSync(require('path').join(__dirname, '../../examples/bundle.js'), src, 'utf-8')
-      test(name, function(t) {
-        vm.runInNewContext(src, { test: t.test.bind(t), window: {} });
-      })
+    .bundle(function (err, src) {
+      if (err) return cb(err);
+      cb(null, { src: src, name: name });
+    });
+}
+
+// run the compiled tests in a new context
+function run(args) {
+  var name = args.name;
+  var src  = args.src;
+
+  test(name, function(t) {
+    vm.runInNewContext(src, { test: t.test.bind(t), window: {} });
+  })
+}
+
+// compile all tests and fire a callback when the
+// all code is compiled and ready to be executed
+function compileAll(tests, cb) {
+  var results = [];
+  var pending = tests.length;
+  tests.forEach(function (name, idx) {
+    compile(name, function (err, result) {
+      if (err) return cb(err);
+
+      results[idx] = result;
+      if (--pending === 0) {
+        cb(null, results);
+      }
+    });
   });
 }
 
-run('independent-overrides')
-run('manipulating-overrides')
-run('noCallThru')
-run('argument-validation')
+var clientTests = [
+    'independent-overrides'
+  , 'manipulating-overrides'
+  , 'noCallThru'
+  , 'argument-validation'
+];
+
+compileAll(clientTests, function (err, results) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+    return;
+  }
+  results.forEach(run);
+});
